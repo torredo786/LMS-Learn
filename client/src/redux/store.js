0194e7f3c91cfa35a1850184e397c0cd92919b5e
Saif <saif@edusmart.com>
2024-11-28 17:01:16 +0530

    saving store to localstorage

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,18 +1,32 @@
-import { createStore, applyMiddleware,compose } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import createSagaMiddleware from "redux-saga";
 import rootSaga from "./sagas/rootSaga";
-import { combineReducers } from 'redux';
-import studentReducer from './reducers/studentReducer';
+import { combineReducers } from "redux";
+import studentReducer from "./reducers/studentReducer";
+import { loadState, saveState } from "../utils/localStorage";
 
 const rootReducer = combineReducers({
-    student: studentReducer,
-  });
+  student: studentReducer,
+});
 
+// Create saga middleware
 const sagaMiddleware = createSagaMiddleware();
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(sagaMiddleware)));
+// Create store with persisted state
+const persistedState = loadState();
+const store = createStore(
+  rootReducer,
+  persistedState, // Initialize with persisted state
+  composeEnhancers(applyMiddleware(sagaMiddleware))
+);
 
+// Subscribe to store updates to save the state
+store.subscribe(() => {
+  saveState(store.getState());
+});
+
+// Run sagas
 sagaMiddleware.run(rootSaga);
 
 export default store;
--- client/src/utils/localStorage.js
+++ client/src/utils/localStorage.js
@@ -1,19 +1,22 @@
-export const loadLocalState = () => {
-    try {
-      const storeDataState = localStorage.getItem("storeData");
-      if (storeDataState === null) return undefined;
-      return JSON.parse(storeDataState);
-    } catch (error) {
-      return undefined;
+export const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem("reduxState");
+    if (serializedState === null) {
+      return undefined; // No state found in localStorage
     }
-  };
-  
-  export const saveLocalState = (state) => {
-    try {
-      const storeDataState = JSON.stringify(state);
-      localStorage.setItem("storeData", storeDataState);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  
+    return JSON.parse(serializedState);
+  } catch (error) {
+    console.error("Failed to load state from localStorage:", error);
+    return undefined;
+  }
+};
+
+// Save state to localStorage
+export const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("reduxState", serializedState);
+  } catch (error) {
+    console.error("Failed to save state to localStorage:", error);
+  }
+};
